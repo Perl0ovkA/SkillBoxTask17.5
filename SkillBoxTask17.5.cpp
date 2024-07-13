6b7452f7d28e894c2e2a8e1ecc7e09160eb8600a
Perl0ovkA <104366771+Perl0ovkA@users.noreply.github.com>
2024-07-13 13:38:20 +0300

    Добавьте файлы проекта.

---
+++ SkillBoxTask17.5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+class Vector
+{
+private:
+	double x;
+	double y;
+	double z;
+public:
+	Vector() : x(0),y(0),z(0)
+	{}
+
+	Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+	{}
+
+
+int Show()
+	{
+	int V = (x + y + z);
+	double LV = pow(V, 2);
+		cout << "\n" << x << " " << y << " " << z << endl << sqrt(LV) << endl;
+
+		return sqrt(LV);
+	}
+};
+
+
+int main()
+{
+	Vector V(3, 3, 3);
+	V.Show();
+}
